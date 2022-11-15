5f2b4f0a483f4db8e4a4bcc991a28456f331bbc5
Kenedy <kenedygarcia43@gmail.com>
2022-11-15 17:21:26 -0600

    1.1

--- app/src/main/java/com/example/recetariob1/Solicitudes.java
+++ app/src/main/java/com/example/recetariob1/Solicitudes.java
@@ -5,9 +5,9 @@ import android.content.Context;
 import com.example.recetariob1.Listeners.RRL;
 import com.example.recetariob1.Models.RandomRecipe;
 
-import java.util.Random;
-
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
@@ -26,7 +26,22 @@ public class Solicitudes {
 
     public void getRandomRecipes(RRL listener) {
         RecetaAleatoria recetaAleatoria = retrofit.create(RecetaAleatoria.class);
-        Call<RandomRecipe> call = recetaAleatoria.callRandomRecipe(context.getString(R.string.api_key), number:"10");
+        Call<RandomRecipe> call = recetaAleatoria.callRandomRecipe(context.getString(R.string.api_key), "10");
+        call.enqueue(new Callback<RandomRecipe>() {
+            @Override
+            public void onResponse(Call<RandomRecipe> call, Response<RandomRecipe> response) {
+                if (!response.isSuccessful()){
+                    listener.didError(response.message());
+                    return;
+                }
+                listener.didFetch(response.body(), response.message());
+            }
+
+            @Override
+            public void onFailure(Call<RandomRecipe> call, Throwable t) {
+                listener.didError(t.getMessage());
+            }
+        });
     }
 
     private interface RecetaAleatoria {
